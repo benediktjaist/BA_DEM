4b725277d42a4d728420cbfc651723216e2f1b1c
benediktjaist <benedikt.jaist@gmail.com>
2022-10-12 22:35:40 +0200

    jupyter test

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from particle_class import particle
 
 p1 = particle(0,1,10,4)
 p2 = particle(5,6,10,5)
+p3 = particle(3,4,3,5)
 
 #initialization
 dt = 0.5
@@ -20,10 +21,18 @@ np=1
 velocity = 1
 acc=1
 force=0
-list_of_particles=[]
-list_of_particles.append(p1)
-list_of_particles.append(p2)
+#list_of_particles=[]
+#list_of_particles.append(p1)
+#list_of_particles.append(p2)
 
+
+print(particle.list_of_particles)
+
+for n_particle in particle.list_of_particles:
+    print(n_particle.position)
+
+"""
+quit()
 #timeloop
 for t in range(simtime):
     #loop of particle
@@ -39,9 +48,15 @@ for t in range(simtime):
         #contact forces
         f_n_e = interpenetration * p1.elstiffnesn
         
+        for n_particle in list_of_particles:
         #integration of motion with verlocity verlet
         new_vel_05 = velocity + 0.5*dt*acc
-        new_pos = p1.position + dt*new_vel_05
+        new_pos = n_particle.position + dt*new_vel_05
         new_force = force
         new_acc = new_force/p1.mass
         new_vel = new_vel_05 + 0.5*dt*new_acc
+        
+        """
+        
+        
+        
--- particle_class.py
+++ particle_class.py
@@ -6,9 +6,16 @@ Created on Wed Oct 12 17:40:25 2022
 """
 
 class particle:
+    list_of_particles=[]
+    
+    
     def __init__(self, position, radius, elstiffnesn, mass):
         self.position = position
         self.radius = radius
         self.elstiffnesn = elstiffnesn
         self.mass = mass
         
+        particle.list_of_particles.append(self)
+        
+       
+        # self.__class__. 
