65b55ea9fda0419b124841308a1617fa9d811abf
benediktjaist <benedikt.jaist@gmail.com>
2022-10-22 13:26:35 +0200

    added new attributes
    rotation and pred_position

--- particle_class.py
+++ particle_class.py
@@ -8,15 +8,16 @@ Created on Wed Oct 12 17:40:25 2022
 class particle:
     list_of_particles=[]
     
-    def __init__(self, position, velocity, acceleration, force, radius, elstiffnesn, mass, pred_posi):
+    def __init__(self, position, velocity, acceleration, rotation, force, radius, elstiffnesn, mass, pred_position):
         self.position = position
         self.velocity = velocity
         self.acceleration = acceleration
+        self.rotation = rotation
         self.force = force
         self.radius = radius
         self.elstiffnesn = elstiffnesn
         self.mass = mass
-        self.pred_posi = pred_posi
+        self.pred_position = pred_position
         
         particle.list_of_particles.append(self)
         
